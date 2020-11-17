8a336a39b475d50dc39522ca6eca8f3439830b00
FlavianMF <flavianmelquiades@gmail.com>
2020-11-17 12:09:06 -0300

    Pinos para constantes

--- PlugPiBlack.h
+++ PlugPiBlack.h
@@ -9,11 +9,11 @@ class PlugPiBlack {
   void setup(void);
 
  private:
-  uint8_t _greenLedPin = 9;
-  uint8_t _redLedPin = 5;
-  uint8_t _yellowLedPin = 10;
-  uint8_t _buzzerPin = 13;
-  uint8_t _ldrPin = 18;
+  const uint8_t _greenLedPin = 9;
+  const uint8_t _redLedPin = 5;
+  const uint8_t _yellowLedPin = 10;
+  const uint8_t _buzzerPin = 13;
+  const uint8_t _ldrPin = 18;
   uint8_t _baudRate = 115200;
 };
 
